f16ad0925a98486a963c00ab715a7bb90a12a3a5
Andrea <andrea.berrocal95@gmail.com>
2022-10-23 01:08:13 -0700

    added handleLogin function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
+import * as ROUTES from "../constants/routes";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -11,7 +12,17 @@ const Login = () => {
   const [error, setError] = useState("");
   const isInvalid = password === "" || emailAddress === "";
 
-  const handleLogin = () => {};
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      await db.auth().signInWithEmailAndPassword(emailAddress, password);
+      navigate(ROUTES.DASHBOARD);
+    } catch (error) {
+      setEmailAddress('');
+      setPassword('');
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
     document.title = "Login - Instagram";
@@ -27,46 +38,49 @@ const Login = () => {
       </div>
       <div className="flex flex-col w-2/5">
         <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
-        <h1 className="flex justify-center w-full">
-          <img
-            src="/images/logo.png"
-            alt="instagram"
-            className="mt-2 w-6/12 mb-4"
-          />
-        </h1>
-        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          <h1 className="flex justify-center w-full">
+            <img
+              src="/images/logo.png"
+              alt="instagram"
+              className="mt-2 w-6/12 mb-4"
+            />
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-        <form onSubmit={handleLogin} method="POST">
-          <input
-            aria-label="Enter your email address"
-            type="text"
-            placeholder="Email Address"
-            className="text-sm text-grey-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({ target }) => setEmailAddress(target.value)}
-          />
-          <input
-            aria-label="Enter your password"
-            type="password"
-            placeholder="Password"
-            className="text-sm text-grey-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          <button
-            disabled={isInvalid}
-            type="submit"
-            className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${
-              isInvalid && "opacity-50"
-            }`}
-          >
-            Log In
-          </button>
-        </form>
-      </div>
-      <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
-        <p className="text-sm">Don't have an account?{` `}
-        <Link to='/signup' className="font-bold text-blue-medium">Sign up</Link>
-        </p>
-      </div>
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email Address"
+              className="text-sm text-grey-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-grey-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${
+                isInvalid && "opacity-50"
+              }`}
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
+          <p className="text-sm">
+            Don't have an account?{` `}
+            <Link to="/signup" className="font-bold text-blue-medium">
+              Sign up
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
