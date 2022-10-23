c090a25c08db3c07c68fd678f4798163e1767478
Andrea <andrea.berrocal95@gmail.com>
2022-10-22 23:20:15 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import * as ROUTES from './constants/routes';
+
+//use lazy loading to get page when requested
+const Login = lazy(() => import ('./pages/Login'))
+
 function App() {
   return (
-    <p>Hello</p>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+      <Routes>
+        <Route path={ROUTES.LOGIN} element={<Login />}/>
+      </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
